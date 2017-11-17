8bdbc9348b63cf60c990aecfcbf20f143e9d60e4
Tom Doyle <tfdoyleiv@gmail.com>
2017-11-17 16:00:17 -0600

    Fixed console display errors, added player information gets, rochambeau choices for 2 players or random for CPU, flavor text

--- rPSLSp/rPSLSp/game.cs
+++ rPSLSp/rPSLSp/game.cs
@@ -10,16 +10,22 @@ namespace rPSLSp
     {
         public int playerOneScore;
         public int playerTwoScore;
-        public Player playerOne = new Player("Player", false);
-        public Player playerTwo = new Player("Computer", false);
+        public Player playerOne = new Player("Player", false, 0);
+        public Player playerTwo = new Player("HAL 9000", false, 0);
         public Game (int playerOneScore, int playerTwoScore)
         {
             this.playerOneScore = playerOneScore;
             this.playerTwoScore = playerTwoScore;
         }
+        public void BeginGame()
+        {
+            StartGame();
+            SelectOpponent();
+            SetGame();
+        }
         public void StartGame()
         {
-            Console.WriteLine("Would you like to play? Press Y/N");
+            Console.WriteLine("\r\nDare you play? Press Y/N");
             ConsoleKeyInfo info = Console.ReadKey();
             if ((info.KeyChar == 'Y') || (info.KeyChar == 'y'))
             {
@@ -28,11 +34,48 @@ namespace rPSLSp
             }
             else if ((info.KeyChar == 'N') || (info.KeyChar == 'n'))
             {
+                Console.WriteLine("\r\n\r\nCoward, thou. Get thee hence, knave!");
+                Console.ReadKey();
+                Environment.Exit(0);
             }
         }
-        //public void DetermineOutcome()
+        public void SelectOpponent()
+        {
+            Console.WriteLine("\r\nIs this battle versus MAN or MACHINE?");
+            string info = Console.ReadLine();
+            if (info.ToLower().Equals("man"))
+            {
+                playerTwo.isHuman = true;
+                playerTwo.GetName();
+                Console.WriteLine("\r\nWe GODS OF ROCHAMBEAU are pleased, mortals.\r\n\r\n" + playerOne.name + "...\r\n\r\n" + playerTwo.name + "...\r\n\r\nprepare thyselves for...\r\n\r\nROCK PAPER SCISSORS LIZARD SPOCK!");
+            }
+            else if (info.ToLower().Equals("machine"))
+            {
+                Console.WriteLine("\r\n" + playerOne.name + ", you have elected to contest our MACHINE. " + playerOne.name + ", prepare thyself, the " + playerTwo.name + " awaits.\r\n\r\n We grow impatient, begin...\r\n\r\n ROCK PAPER SCISSORS LIZARD SPOCK!");
+            }
+            else
+            {
+                Console.WriteLine("\r\nDo not test our patience. That is not an option, mortal. Choose again.");
+                SelectOpponent();
+            }
+        }
+        public void SetGame()
+        {
+            if (playerTwo.isHuman == true)
+            {
+                Console.WriteLine("\r\nHonor among thieves. DO NOT peek at your opponent entering their choice!\r\n");
+                playerOne.ChooseMove();
+                playerTwo.ChooseMove();
+            }
+            else
+            {
+                playerOne.ChooseMove();
+                playerTwo.ChooseMove();
+            }
+        }
+        //public void DecideOutcome()
         //{
-           // determination = (5 + aChoice - bChoice) % 5;
+           // decision = (5 + playerOne.playerChoice - playerTwo.playerChoice) % 5;
         //}
     }
 }
--- rPSLSp/rPSLSp/player.cs
+++ rPSLSp/rPSLSp/player.cs
@@ -10,16 +10,55 @@ namespace rPSLSp
     {
         public string name;
         public bool isHuman;
-        public Player(string name, bool isHuman)
+        public int playerChoice;
+        Random randomChoice = new Random();
+        public Player(string name, bool isHuman, int playerChoice)
         {
             this.name = name;
             this.isHuman = isHuman;
+            this.playerChoice = playerChoice;
         }
         public void GetName()
         {
-            Console.WriteLine("\r\nWhat is your name, bold champion?");
+            Console.WriteLine("\r\n\r\nWhat is your name, bold champion of ROCHAMBEAU?");
             name = Console.ReadLine();
         }
-
+        public void ChooseMove()
+        {
+            if (isHuman == true)
+            {
+                Console.WriteLine("\r\n" + name + ", choose your move.\r\n\r\nROCK,\r\nPAPER,\r\nSCISSORS,\r\nLIZARD,\r\n or SPOCK.");
+                string choice = Console.ReadLine();
+                if (choice.ToLower().Equals("rock"))
+                {
+                    playerChoice = 0;
+                }
+                else if (choice.ToLower().Equals("paper"))
+                {
+                    playerChoice = 1;
+                }
+                else if (choice.ToLower().Equals("scissors"))
+                {
+                    playerChoice = 2;
+                }
+                else if (choice.ToLower().Equals("lizard"))
+                {
+                    playerChoice = 3;
+                }
+                else if (choice.ToLower().Equals("spock"))
+                {
+                    playerChoice = 4;
+                }
+                else
+                {
+                    Console.WriteLine("\r\n" + name + ", you dare mock the GODS OF ROCHAMBEAU? Enter a proper choice.");
+                    ChooseMove();
+                }
+            }
+            else
+            {
+                playerChoice = randomChoice.Next(0, 4);
+            }
+        }
     }
 }
